9f4f2ba7bef29fa5d29c38865d2347e8bb38d6cb
BytEBikerDistro <jbumagat125@gmail.com>
2025-10-16 15:02:08 +0800

    Create code_runner.lua

---
+++ lua/plugins/code_runner.lua
@@ -0,0 +1,14 @@
+-- ~/.config/nvim/lua/plugins/code_runner.lua
+
+return {
+    "CRAG666/code_runner.nvim",
+    cmd = {
+        "RunCode",
+        "RunFile",
+        "RunProject",
+        "RunClose",
+        "CRFiletype",
+        "CRProjects",
+    },
+    config = true
+}
