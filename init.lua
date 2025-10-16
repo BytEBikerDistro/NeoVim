1a06b36441d779f0ef3d9eda3bdb024cb248883d
BytEBikerDistro <jbumagat125@gmail.com>
2025-10-16 13:28:16 +0800

    Initialize Neovim configuration with lazy.nvim

---
+++ init.lua
@@ -0,0 +1,23 @@
+-- ~/.config/nvim/init.lua
+
+-- Bootstrap lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable",
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Load core configuration
+require("core.options")
+require("core.keymaps")
+require("core.autocommands")
+
+-- Load plugins
+require("plugins")
