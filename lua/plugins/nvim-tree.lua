77ff4d86aacf620cbc0a8fd3d5926942dc101ad4
BytEBikerDistro <jbumagat125@gmail.com>
2025-10-16 14:40:02 +0800

    Update nvim-tree.lua

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,26 +1,74 @@
 -- ~/.config/nvim/lua/plugins/nvim-tree.lua
 
 return {
-  "nvim-tree/nvim-tree.lua",
-  dependencies = {
-    "nvim-tree/nvim-web-devicons",
-  },
-  config = function()
-    require("nvim-tree").setup({
-      view = {
-        width = 35,
-      },
-      renderer = {
-        group_empty = true,
-      },
-      filters = {
-        dotfiles = false,
-      },
-    })
-    
-    -- Keymaps
-    vim.keymap.set("n", "<C-n>", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file tree" })
-    vim.keymap.set("v", "<C-n>", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file tree" })
-    vim.keymap.set("i", "<C-n>", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file tree" })
-  end,
+    "nvim-tree/nvim-tree.lua",
+    priority = 1000,
+    cmd = { "NvimTreeToggle", "NvimTreeFocus" },
+    opts = {
+        filters = {
+            git_ignored = false,
+            custom = { "^.git$", ".DS_Store", "thumbs.db", "^.hidden$" },
+        },
+        live_filter = {
+            prefix = "[FILTER]: ",
+            always_show_folders = false,
+        },
+        disable_netrw = true,
+        hijack_cursor = true,
+        sync_root_with_cwd = true,
+        update_focused_file = {
+            enable = true,
+            update_root = false,
+        },
+        view = {
+            width = 35,
+            preserve_window_proportions = true,
+        },
+        git = {
+            enable = true,
+            ignore = false,
+        },
+        renderer = {
+            add_trailing = true,
+            highlight_git = "all",
+            highlight_opened_files = "icon",
+            indent_markers = { enable = true },
+            icons = {
+                glyphs = {
+                    default = "󰈚",
+                    bookmark = "󰆤",
+                    modified = "●",
+                    hidden = "󰜌",
+                    folder = {
+                        default = "",
+                        empty = "",
+                        empty_open = "",
+                        open = "",
+                        symlink = "",
+                    },
+                    git = {
+                        unstaged = "✗",
+                        staged = "✓",
+                        unmerged = "",
+                        renamed = "➜",
+                        untracked = "★",
+                        deleted = "",
+                        ignored = "◌",
+                    },
+                },
+            },
+        },
+    },
+    -- config = function(_, opts)
+    --     require("nvim-tree").setup(opts)
+    --
+    --     -- Statusline tweak for tree window
+    --     local nt_api = require("nvim-tree.api")
+    --     nt_api.events.subscribe(nt_api.events.Event.TreeOpen, function()
+    --         local tree_winid = nt_api.tree.winid()
+    --         if tree_winid ~= nil then
+    --             vim.api.nvim_set_option_value("statusline", "%t", { win = tree_winid })
+    --         end
+    --     end)
+    -- end,
 }
