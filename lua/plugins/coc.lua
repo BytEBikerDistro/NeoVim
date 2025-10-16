bacf42e8409ad4a09b6043fea1bb2e7ff9db40f0
BytEBikerDistro <jbumagat125@gmail.com>
2025-10-16 14:32:38 +0800

    Update coc.lua

--- lua/plugins/coc.lua
+++ lua/plugins/coc.lua
@@ -6,50 +6,61 @@ return {
   config = function()
     -- Coc configuration will be loaded from coc-settings.json
     
-    -- Keymaps
-    vim.keymap.set("n", "gd", "<Plug>(coc-definition)", { desc = "Go to definition" })
-    vim.keymap.set("n", "gy", "<Plug>(coc-type-definition)", { desc = "Go to type definition" })
-    vim.keymap.set("n", "gi", "<Plug>(coc-implementation)", { desc = "Go to implementation" })
-    vim.keymap.set("n", "gr", "<Plug>(coc-references)", { desc = "Show references" })
-    
-    vim.keymap.set("n", "K", function()
-      local filetype = vim.bo.filetype
-      if vim.tbl_contains({"vim","help"}, filetype) then
-        vim.cmd("h " .. vim.fn.expand("<cword>"))
-      elseif vim.fn.CocActionAsync("hasProvider", "hover") then
-        vim.fn.CocActionAsync("doHover")
-      else
-        vim.cmd("!" .. vim.o.keywordprg .. " " .. vim.fn.expand("<cword>"))
-      end
-    end, { desc = "Show documentation" })
-    
-    -- Code actions and formatting
-    vim.keymap.set("n", "<leader>ca", "<Plug>(coc-codeaction)", { desc = "Code action" })
-    vim.keymap.set("n", "<leader>fm", "<cmd>CocCommand prettier.formatFile<CR>", { desc = "Format file" })
-    
-    -- Completion
-    vim.keymap.set("i", "<Tab>", function()
-      if vim.fn.pumvisible() == 1 then
-        return "<C-n>"
-      else
-        return "<Tab>"
-      end
-    end, { expr = true })
-    
-    vim.keymap.set("i", "<S-Tab>", function()
-      if vim.fn.pumvisible() == 1 then
-        return "<C-p>"
-      else
-        return "<S-Tab>"
-      end
-    end, { expr = true })
-    
-    vim.keymap.set("i", "<CR>", function()
-      if vim.fn.pumvisible() == 1 then
-        return "<C-y>"
-      else
-        return "<CR>"
-      end
-    end, { expr = true })
+    local keymap = vim.keymap.set
+
+-- Basic Coc keymaps (mirrors your old config)
+keymap('n', 'gd', '<Plug>(coc-definition)', { desc = 'Go to definition' })
+keymap('n', 'gy', '<Plug>(coc-type-definition)', { desc = 'Go to type definition' })
+keymap('n', 'gi', '<Plug>(coc-implementation)', { desc = 'Go to implementation' })
+keymap('n', 'gr', '<Plug>(coc-references)', { desc = 'Show references' })
+
+-- Hover / K behaviour
+keymap('n', 'K', function()
+  local filetype = vim.bo.filetype
+  if vim.tbl_contains({'vim', 'help'}, filetype) then
+    vim.cmd('h ' .. vim.fn.expand('<cword>'))
+  elseif vim.fn.CocActionAsync and vim.fn.CocActionAsync('hasProvider', 'hover') == 1 then
+    vim.fn.CocActionAsync('doHover')
+  else
+    vim.cmd('!' .. vim.o.keywordprg .. ' ' .. vim.fn.expand('<cword>'))
+  end
+end, { desc = 'Show documentation' })
+
+-- Code actions, formatting
+keymap('n', '<leader>ca', '<Plug>(coc-codeaction)', { desc = 'Code action' })
+keymap('n', '<leader>f', '<cmd>CocCommand prettier.formatFile<CR>', { desc = 'Format file' })
+
+-- Completion <Tab> behavior (preserve your original logic)
+keymap('i', '<Tab>', function()
+  if vim.fn.pumvisible() == 1 then
+    return vim.api.nvim_replace_termcodes('<C-n>', true, false, true)
+  else
+    return '<Tab>'
+  end
+end, { expr = true })
+
+keymap('i', '<S-Tab>', function()
+  if vim.fn.pumvisible() == 1 then
+    return vim.api.nvim_replace_termcodes('<C-p>', true, false, true)
+  else
+    return vim.api.nvim_replace_termcodes('<S-Tab>', true, false, true)
+  end
+end, { expr = true })
+
+keymap('i', '<CR>', function()
+  if vim.fn.pumvisible() == 1 then
+    return vim.api.nvim_replace_termcodes('<C-y>', true, false, true)
+  else
+    return '<CR>'
+  end
+end, { expr = true })
+
+-- Autoformat on save for common filetypes using Coc
+vim.api.nvim_create_autocmd('BufWritePre', {
+  pattern = { '*.js', '*.jsx', '*.ts', '*.tsx', '*.json', '*.css', '*.scss', '*.md', '*.yaml', '*.yml' },
+  callback = function()
+    if vim.fn.CocAction('format') then
+      vim.cmd('silent call CocAction("format")')
+    end
   end,
-}
+})
