43ecfaf77e5be5676c1197ae9585cbc26925a20f
BytEBikerDistro <jbumagat125@gmail.com>
2025-10-16 15:32:35 +0800

    Update init.lua

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,44 +1,44 @@
 -- ~/.config/nvim/lua/plugins/init.lua
 
 require("lazy").setup({
-  
+
   -- File explorer
   { import = "plugins.nvim-tree" },
-  
+
   -- Status line
   { import = "plugins.lualine" },
-  
+
   -- Syntax highlighting
   { import = "plugins.treesitter" },
-  
+
   -- Fuzzy finder
   { import = "plugins.telescope" },
-  
+
   -- Theme
   { import = "plugins.tokyonight" },
 
-		-- Dashboard
+-- Dashboard
   { import = "plugins.alpha" },
 
-		-- Buffer line
+-- Buffer line
   { import = "plugins.bufferline" },
 
-		-- Code Runner
+-- Code Runner
   { import = "plugins.code_runner" },
 
-		-- Indentation
+-- Indentation
   { import = "plugins.indentscope" },
 
-		-- Notification
+-- Notification
   { import = "plugins.noice" },
 
-		-- TODO Comments
+-- TODO Comments
   { import = "plugins.to-do" },
 
-		-- Teminal Float
+-- Teminal Float
   { import = "plugins.toggleterm" },
-  
+
   -- LSP and completion
   { import = "plugins.coc" },
-  
+
 })
