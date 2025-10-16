74c86497feeda3fb90dda8991edd7985243ce972
BytEBikerDistro <jbumagat125@gmail.com>
2025-10-16 13:39:05 +0800

    Add Treesitter configuration for Neovim

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,20 @@
+-- ~/.config/nvim/lua/plugins/treesitter.lua
+
+return {
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function()
+    require("nvim-treesitter.configs").setup({
+      ensure_installed = {
+        "lua", "vim", "vimdoc", "javascript", "typescript", 
+        "python", "html", "css", "json", "yaml", "markdown"
+      },
+      sync_install = false,
+      auto_install = true,
+      highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+      },
+    })
+  end,
+}
