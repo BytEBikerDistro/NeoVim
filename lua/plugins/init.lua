f597ff967d31108602c8ced475104870bb4ad4f0
BytEBikerDistro <jbumagat125@gmail.com>
2025-10-16 15:28:58 +0800

    Update init.lua

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -16,6 +16,27 @@ require("lazy").setup({
   
   -- Theme
   { import = "plugins.tokyonight" },
+
+		-- Dashboard
+		{ import = "plugins.alpha" },
+
+		-- Buffer line
+  { import = "plugins.bufferline" },
+
+		-- Code Runner
+  { import = "plugins.code_runner" },
+
+		-- Indentation
+  { import = "plugins.indentscope" },
+
+		-- Notification
+  { import = "plugins.noice" },
+
+		-- TODO Comments
+  { import = "plugins.to-do" },
+
+		-- Teminal Float
+  { import = "plugins.toggleterm" },
   
   -- LSP and completion
   { import = "plugins.coc" },
