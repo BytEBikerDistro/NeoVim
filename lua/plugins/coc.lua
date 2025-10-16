4aacf56a5d497b36463afa70f4b6086c9645ad24
BytEBikerDistro <jbumagat125@gmail.com>
2025-10-16 16:02:28 +0800

    Update coc.lua

--- lua/plugins/coc.lua
+++ lua/plugins/coc.lua
@@ -6,61 +6,86 @@ return {
   config = function()
     -- Coc configuration will be loaded from coc-settings.json
 
-	local keymap = vim.keymap.set
+    local keymap = vim.keymap.set
 
--- Basic Coc keymaps (mirrors your old config)
-keymap('n', 'gd', '<Plug>(coc-definition)', { desc = 'Go to definition' })
-keymap('n', 'gy', '<Plug>(coc-type-definition)', { desc = 'Go to type definition' })
-keymap('n', 'gi', '<Plug>(coc-implementation)', { desc = 'Go to implementation' })
-keymap('n', 'gr', '<Plug>(coc-references)', { desc = 'Show references' })
+    -- Basic Coc keymaps
+    keymap('n', 'gd', '<Plug>(coc-definition)', { desc = 'Go to definition' })
+    keymap('n', 'gy', '<Plug>(coc-type-definition)', { desc = 'Go to type definition' })
+    keymap('n', 'gi', '<Plug>(coc-implementation)', { desc = 'Go to implementation' })
+    keymap('n', 'gr', '<Plug>(coc-references)', { desc = 'Show references' })
 
--- Hover / K behaviour
-keymap('n', 'K', function()
-  local filetype = vim.bo.filetype
-  if vim.tbl_contains({'vim', 'help'}, filetype) then
-    vim.cmd('h ' .. vim.fn.expand('<cword>'))
-  elseif vim.fn.CocActionAsync and vim.fn.CocActionAsync('hasProvider', 'hover') == 1 then
-    vim.fn.CocActionAsync('doHover')
-  else
-    vim.cmd('!' .. vim.o.keywordprg .. ' ' .. vim.fn.expand('<cword>'))
-  end
-end, { desc = 'Show documentation' })
+    -- Enhanced hover behavior
+    keymap('n', 'K', function()
+      local filetype = vim.bo.filetype
+      if vim.tbl_contains({ 'vim', 'help' }, filetype) then
+        vim.cmd('h ' .. vim.fn.expand('<cword>'))
+      else
+        local ok, _ = pcall(vim.fn.CocAction, 'doHover')
+        if not ok then
+          vim.cmd('normal! ' .. vim.o.keywordprg .. vim.fn.expand('<cword>'))
+        end
+      end
+    end, { desc = 'Show documentation' })
 
--- Code actions, formatting
-keymap('n', '<leader>ca', '<Plug>(coc-codeaction)', { desc = 'Code action' })
-keymap('n', '<leader>fm', '<cmd>CocCommand prettier.formatFile<CR>', { desc = 'Format file' })
+    -- Code actions and formatting
+    keymap('n', '<leader>ca', '<Plug>(coc-codeaction)', { desc = 'Code action' })
+    keymap('n', '<leader>fm', '<cmd>call CocAction("format")<CR>', { desc = 'Format file' })
 
--- Completion <Tab> behavior (preserve your original logic)
-keymap('i', '<Tab>', function()
-  if vim.fn.pumvisible() == 1 then
-    return vim.api.nvim_replace_termcodes('<C-n>', true, false, true)
-  else
-    return '<Tab>'
-  end
-end, { expr = true })
+    -- Smart completion behavior
+    keymap('i', '<Tab>', function()
+      if vim.fn.pumvisible() == 1 then
+        return vim.fn['coc#pum#visible']() and vim.fn['coc#pum#next'](1) or '<Tab>'
+      else
+        return vim.fn['coc#expandableOrJumpable']() and '<C-r>=coc#rpc#request("doKeymap", ["snippets-expand-jump",""])<CR>' or '<Tab>'
+      end
+    end, { expr = true })
 
-keymap('i', '<S-Tab>', function()
-  if vim.fn.pumvisible() == 1 then
-    return vim.api.nvim_replace_termcodes('<C-p>', true, false, true)
-  else
-    return vim.api.nvim_replace_termcodes('<S-Tab>', true, false, true)
-  end
-end, { expr = true })
+    keymap('i', '<S-Tab>', function()
+      if vim.fn.pumvisible() == 1 then
+        return vim.fn['coc#pum#visible']() and vim.fn['coc#pum#prev'](1) or '<S-Tab>'
+      else
+        return '<S-Tab>'
+      end
+    end, { expr = true })
 
-keymap('i', '<CR>', function()
-  if vim.fn.pumvisible() == 1 then
-    return vim.api.nvim_replace_termcodes('<C-y>', true, false, true)
-  else
-    return '<CR>'
-  end
-end, { expr = true })
+    keymap('i', '<CR>', function()
+      if vim.fn.pumvisible() == 1 then
+        return vim.fn['coc#pum#visible']() and vim.fn['coc#pum#confirm']() or '<C-g>u<CR><C-r>=coc#on_enter()<CR>'
+      else
+        return '<C-g>u<CR><C-r>=coc#on_enter()<CR>'
+      end
+    end, { expr = true })
 
--- Autoformat on save for common filetypes using Coc
-vim.api.nvim_create_autocmd('BufWritePre', {
-  pattern = { '*.js', '*.jsx', '*.ts', '*.tsx', '*.json', '*.css', '*.scss', '*.md', '*.yaml', '*.yml' },
-  callback = function()
-    if vim.fn.CocAction('format') then
-      vim.cmd('silent call CocAction("format")')
-    end
+    -- Use <c-space> to trigger completion
+    keymap('i', '<c-space>', 'coc#refresh()', { expr = true })
+
+    -- Autoformat on save
+    vim.api.nvim_create_autocmd('BufWritePre', {
+      pattern = { '*.js', '*.jsx', '*.ts', '*.tsx', '*.json', '*.css', '*.scss', '*.md', '*.yaml', '*.yml' },
+      callback = function()
+        -- Check if Coc formatting is available
+        if vim.fn.CocHasProvider('formatter') == 1 then
+          vim.fn.CocAction('format')
+        end
+      end,
+    })
+
+    -- Highlight symbol under cursor
+    vim.api.nvim_create_autocmd('CursorHold', {
+      callback = function()
+        if vim.fn.CocHasProvider('documentHighlight') == 1 then
+          vim.fn.CocActionAsync('highlight')
+        end
+      end,
+    })
+
+    -- Clean up highlights when cursor moves
+    vim.api.nvim_create_autocmd('CursorMoved', {
+      callback = function()
+        if vim.fn.CocHasProvider('documentHighlight') == 1 then
+          vim.fn.CocActionAsync('clearHighlight')
+        end
+      end,
+    })
   end,
-})
+}
